3f11ebce0c0b0260d1274452ccbe9fb9471daa6b
avisionx <avi17223@iiitd.ac.in>
2020-08-17 18:35:01 +0000

    Deploying to gh-pages from  @ 54b96e2bbc1b406e0d09147a429679c842e1d2c5 🚀

---
+++ precache-manifest.5820a755580bcb0706a447e2a27930bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26191af1add6318974d2555706c01be3",
+    "url": "/lingualytics/index.html"
+  },
+  {
+    "revision": "ce9f3865a6ca2746abc0",
+    "url": "/lingualytics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a33196c57db98418e92",
+    "url": "/lingualytics/static/js/2.56eaa85b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lingualytics/static/js/2.56eaa85b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9f3865a6ca2746abc0",
+    "url": "/lingualytics/static/js/main.4c122e04.chunk.js"
+  },
+  {
+    "revision": "c33ed03ad7430ef5735a",
+    "url": "/lingualytics/static/js/runtime-main.1d106745.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lingualytics/static/media/logo.5d5d9eef.svg"
+  }
+]);
