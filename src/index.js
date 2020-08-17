0c388a3bbf8a9928254c47c3bd91b2ae30b2f4db
avisionx <avisionx@Razor.localdomain>
2020-08-18 00:12:13 +0530

    fix url

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>,
+  </React>,
   document.getElementById('root')
 );
 
