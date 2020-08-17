e5dbb4dde7aedef5774fc70cafd40cf1de53b8f8
avisionx <avi.grg4567@gmail.com>
2020-08-18 00:16:39 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React>
-    <App />
-  </React>,
+    <App />,
   document.getElementById('root')
 );
 
